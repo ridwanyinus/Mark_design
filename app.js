fb48127e73d5ba41400ae3368efeb1685d58fd36
officialyaro9 <ridwanyinusa15@gmail.com>
2023-11-13 11:49:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburger-button").addEventListener("click", function () {
+   document.querySelector(".nav-links").style.display = document.querySelector(".nav-links").style.display == "none" ? "block" : "none";
+});
